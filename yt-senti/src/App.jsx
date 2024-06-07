4ac86556a7dfce73aa50b9da66a94a96605bcf4e
Amaan <121273095+AmaanBilwar@users.noreply.github.com>
2024-06-07 17:12:17 -0400

    added basic func

--- yt-senti/src/App.jsx
+++ yt-senti/src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import './App.css'
+import basicFunc from './basicFunc'
 
-const App = () => {
+
+
+function App() {
+
+  
   return (
     <>
       
 <h1>Youtube Sentiment analysis</h1>
-
+<div >
+  <form >
+    <input  type='url' placeholder='Enter the link here' />
+  </form>
+  </div>
+  <div id='button'>
+    <button id='test' onClick={basicFunc()} className='p-2 rounded bg-orange-50'>Submit</button>
+    </div>
     </>
   )
 }
---
+++ yt-senti/src/basicFunc.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function basicFunc(){
+    console.log('button works')
+}
+
+export default basicFunc;
